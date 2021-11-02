8e1891d9a8d2d7f6568d5c00c359cadb472560ac
Karita Laine <44746@edu.raseko.fi>
2021-11-02 13:13:58 +0200

    Viewsien luomista
    Työstetty myös headeria ja footeria

--- autokorjaamo/base/urls.py
+++ autokorjaamo/base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.etusivu, name='etusivu'), 
+    path('palvelut/', views.palvelut, name='palvelut'),
+    path('yhteystiedot/', views.yhteystiedot, name='yhteystiedot'),
+    path('palaute/', views.palaute, name='palaute'),
 ]
