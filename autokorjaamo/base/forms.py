f45cc8873fdd039abd8d2bb5ed61e4c2d69aea57
Verneri <44767@edu.raseko.fi>
2021-11-10 15:45:49 +0200

    muotoiltiin palautelomaketta ja korjattiin form syntaxia

--- autokorjaamo/base/forms.py
+++ autokorjaamo/base/forms.py
@@ -3,12 +3,15 @@ from django.forms import ModelForm
 from .models import Palaute
 
 
-class PalauteLomake(ModelForm):
+class PalauteLomake(forms.ModelForm):
     class Meta:
         model = Palaute
         fields = ('nimi', 'palaute')
-
+        labels = {
+            'nimi': '',
+            'palaute': ''
+        }
         widgets = {
-            'nimi': forms.TextInput(attrs={'class':'form-control'})
-
+            'nimi': forms.TextInput(attrs={'placeholder': 'Nimi...', 'class': 'palaute_nimi'}),
+            'palaute': forms.Textarea(attrs={'placeholder': 'Anna palautetta...', 'class': 'palaute_textarea'})
         }
--- autokorjaamo/base/models.py
+++ autokorjaamo/base/models.py
@@ -4,6 +4,9 @@ class Palaute(models.Model):
     nimi = models.CharField(max_length=50)
     palaute = models.TextField(max_length=1000)
 
+    def __str__(self):
+        return self.palaute
+        
 class Palvelu(models.Model):
     palvelu = models.CharField(max_length=200)
     hinta = models.CharField(max_length=200)
