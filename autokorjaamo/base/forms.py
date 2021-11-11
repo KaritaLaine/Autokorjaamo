7a9f49fda2f3206d04b491b4bb98d12a7a31f2f1
Verneri <44767@edu.raseko.fi>
2021-11-11 10:13:13 +0200

    lisättiin sposti kenttä

--- autokorjaamo/base/forms.py
+++ autokorjaamo/base/forms.py
@@ -6,12 +6,14 @@ from .models import Palaute
 class PalauteLomake(forms.ModelForm):
     class Meta:
         model = Palaute
-        fields = ('nimi', 'palaute')
+        fields = ('nimi', 'sposti', 'palaute')
         labels = {
             'nimi': '',
+            'sposti': '',
             'palaute': ''
         }
         widgets = {
             'nimi': forms.TextInput(attrs={'placeholder': 'Nimi...', 'class': 'palaute_nimi'}),
-            'palaute': forms.Textarea(attrs={'placeholder': 'Anna palautetta...', 'class': 'palaute_textarea'})
+            'palaute': forms.Textarea(attrs={'placeholder': 'Anna palautetta...', 'class': 'palaute_textarea'}),
+            'sposti': forms.EmailInput(attrs={'placeholder': 'Sähköposti...', 'class': 'palaute_sposti'})
         }
--- autokorjaamo/base/models.py
+++ autokorjaamo/base/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Palaute(models.Model):
     nimi = models.CharField(max_length=50)
     palaute = models.TextField(max_length=1000)
+    sposti = models.EmailField(max_length=150)
 
     def __str__(self):
         return self.palaute
